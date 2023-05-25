8fac6da1b590f91bda2105f7478e149544f03374
sivagamasrinivasan <784108@pdsb.net>
2023-05-25 14:31:17 -0400

    version 1

---
+++ src/week3example/Week3Example.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3example;
+
+/**
+ *
+ * @author srinivsi
+ */
+public class Week3Example {
+
+     /*PUSH steps 1. initilize versioning , .gitconfig file, .gitignore, Local Repository
+             commit*/
+    public static void main(String[] args) 
+    {
+    System.out.println("learning github operations");
+    }
+    
+}
